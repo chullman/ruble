f7af8fa7610a392b6d48a6a8bd353cfdf12e3ec6
Christopher Hullman <christopherhullman@gmail.com>
2022-04-14 13:55:46 +1000

    script added to extract all 5 letters words from wordsAPI - PRIVATE KEY EXCLUDED

---
+++ src/strip_source_dict_wordsapi.rb
@@ -0,0 +1,56 @@
+# https://rapidapi.com/dpventures/api/wordsapi/
+
+require 'uri'
+require 'net/http'
+require 'openssl'
+require 'json'
+
+
+def fetch(uri_str, limit = 10)
+    
+    raise ArgumentError, 'too many HTTP redirects' if limit == 0
+  
+    http = Net::HTTP.new(uri_str.host, uri_str.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(uri_str)
+    request["X-RapidAPI-Host"] = 'wordsapiv1.p.rapidapi.com'
+    request["X-RapidAPI-Key"] = '(TO ADD)'
+
+    response = http.request(request)
+  
+    case response
+    when Net::HTTPSuccess then
+
+        full_hash = JSON.parse(response.body)
+        (full_hash["results"])["data"]
+      
+      
+    when Net::HTTPRedirection then
+      location = response['location']
+      warn "redirected to #{location}"
+      fetch(location, limit - 1)
+    else
+      response.value
+    end
+end
+
+
+def fetch_page_results(page_counter)
+    api_page_uri = URI("https://wordsapiv1.p.rapidapi.com/words/?letters=5&page=#{page_counter}")
+    api_page_results = fetch(api_page_uri)
+    api_page_results
+end
+
+all_five_letter_words = []
+
+page_counter = 1
+while !(fetch_page_results(page_counter).empty?) do
+    puts "Processing page #{page_counter}..."
+    fetch_page_results(page_counter).each do |word|
+        all_five_letter_words.push(word)
+    end
+    page_counter += 1
+end
+
