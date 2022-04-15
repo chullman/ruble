bfc8896ca3b57f2f31d9b97d959cd11ed2e023c0
Christopher Hullman <christopherhullman@gmail.com>
2022-04-15 20:53:47 +1000

    json handler class implemented and tested with error handled - needs to be moved into own file

--- src/feature_one/filehandler.rb
+++ src/feature_one/filehandler.rb
@@ -51,11 +51,41 @@ class FileHandler
   
 end
 
+require 'json'
+
+class JSONHandler
+    def initialize
+    end
+
+    def try_parse_json(json_string)
+        begin
+            json_results = JSON.parse(json_string)
+        rescue JSON::ParserError => e
+            puts "ERROR: The string argument passed into try_parse_json method was unable to be parsed as valid JSON"
+            puts "ERROR @: #{e.backtrace[2]}"
+            return nil
+        else
+            if json_results.empty?
+                puts "ERROR: The JSON syntax is valid, but it is empty of content"
+                return nil
+            else
+                return json_results
+            end
+        end
+    end
+
+end
+
   
 file_handler = FileHandler.new
 
 file = file_handler.try_file_open("../misc_feature/strip_source_dict_wordnet.json")
 
-output = file_handler.try_file_read(file) unless file.nil?
+output_string = file_handler.try_file_read(file) unless file.nil?
 
-puts output.class
+
+json_handler = JSONHandler.new
+
+json_results = json_handler.try_parse_json(output_string)
+
+puts json_results unless json_results.nil?
