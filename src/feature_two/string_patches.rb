5f29a5363d6d502036c9f79733677019ce1f533c
Christopher Hullman <christopherhullman@gmail.com>
2022-04-16 13:26:56 +1000

    added commenting to explain why we're abstracting the colorize gem'

--- src/feature_two/string_patches.rb
+++ src/feature_two/string_patches.rb
@@ -1,5 +1,7 @@
 # "Monkey patching" Ruby's String class the 'right' way, according to this doc:
 # https://ruby-doc.org/core-2.4.0/doc/syntax/refinements_rdoc.html (viewed 16/04/2022)
+# This will make use of the 'colorize' gem a lot tider throughout our app, as we can just call 'color' method on
+# any string, rather than directly making references the 'colorize' gem's methods instead. Good for encapsulation and abstraction.
 
 require 'colorized_string'
 
