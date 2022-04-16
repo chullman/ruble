63b64f33ad747360386307bda1d4b990a4f93c90
Christopher Hullman <christopherhullman@gmail.com>
2022-04-16 11:15:18 +1000

    finished is_valid_input? method and tested working

--- src/main.rb
+++ src/main.rb
@@ -12,4 +12,35 @@ json_handler = JSONHandler.new
 
 json_results = json_handler.try_parse_json(output_string)
 
-puts json_results unless json_results.nil?
+# puts json_results unless json_results.nil?
+
+def is_valid_input?(input, input_length_restriction, all_dict_words)
+
+    input = input.to_s.strip.downcase
+
+    if input.length != input_length_restriction
+        return false
+    end
+
+    # explanation of this regex and the gsub method: https://stackoverflow.com/a/6344630
+    special = "?<>',?[]}{=-)(*&^%$#`~{}_\""
+    special_char_regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
+
+    if input.include?(" ") || /\d/.match(input) || input =~ special_char_regex
+        return false
+    end
+
+    if !(all_dict_words.include?(input))
+        puts "entered"
+        return false
+    end
+
+    return true
+end
+
+print ": "
+input = gets.chomp
+
+if is_valid_input?(input, 5, json_results)
+    
+end
