31e11854fc5ca23505825cd5c64ccfe2948fecae
Christopher Hullman <christopherhullman@gmail.com>
2022-04-21 14:12:04 +1000

    added error handling if terminal window is too small to display the results

--- src/feature_three/display_results.rb
+++ src/feature_three/display_results.rb
@@ -11,15 +11,20 @@ def display_results(answers_storage)
             if answer_hash[n][1] == :green
                 colorized_answer << "  ".color(:default, :light_green) + answer_hash[n][0].to_s.upcase.color(:black, :light_green) + "  ".color(:default, :light_green)
             elsif answer_hash[n][1] == :orange
-                colorized_answer << "  ".color(:default, :yellow) + answer_hash[n][0].to_s.upcase.color(:light_white, :yellow) + "  ".color(:default, :yellow)
+                colorized_answer << "  ".color(:default, :yellow) + answer_hash[n][0].to_s.upcase.color(:black, :yellow) + "  ".color(:default, :yellow)
             elsif answer_hash[n][1] == :grey
                 colorized_answer << "  ".color(:default, :light_black) + answer_hash[n][0].to_s.upcase.color(:light_white, :light_black) + "  ".color(:default, :light_black)
             end
         end
         table << colorized_answer
-        puts table.render(:unicode, padding: [1,1,1,1])
-        
-    end
 
+        begin
+            puts table.render(:unicode, padding: [1,1,1,1])
+        rescue
+            puts "ERROR: Terminal window width is too small to correctly display the results. Please enlarge your window."
+            puts table.render(:unicode)
+        end
+
+    end
 
 end
